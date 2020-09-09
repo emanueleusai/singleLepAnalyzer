c70947492b2ff54d9d0d65fa9c3a55fb7a21c889
Emanuele Usai <ema2487@gmail.com>
2020-09-09 01:00:34 -0400

    test adds

---
+++ combineLimits/run.sh
@@ -0,0 +1,10 @@
+# cd limits_R$1_40vars_4j_ttHFupLFdown
+cd limits_R17_08262020_66vars_4j
+
+rm cmb/workspace.root
+combineTool.py -M T2W -i cmb/* -o workspace.root --parallel 4
+
+combine -M Significance cmb/workspace.root -t -1 --expectSignal=1 --cminDefaultMinimizerStrategy 0 &> sig.txt
+combine -M AsymptoticLimits cmb/workspace.root --run=blind --cminDefaultMinimizerStrategy 0 &> asy.txt
+
+cd ../
